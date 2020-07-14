237c4c8a74bd5e246b8a125531acb6e1521c10e3
Sharon Kim <sharonkim1709@gmail.com>
2020-07-14 17:39:09 -0400

    clean slate of burgers

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,4 @@
-INSERT INTO burgers(burger_name,devoured) VALUES("Hamburgers", false);
 INSERT INTO burgers(burger_name,devoured) VALUES("Cheese Burger",false);
 INSERT INTO burgers(burger_name,devoured) VALUES("Bacon Cheese Burger",false);
+
+SELECT * FROM burgers;
