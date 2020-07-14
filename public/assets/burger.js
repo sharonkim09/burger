a648b27a90c401459180bcd0158f0dec04e8514b
Sharon Kim <sharonkim1709@gmail.com>
2020-07-14 09:18:07 -0400

    added ajax call and listener for eating burger

--- public/assets/burger.js
+++ public/assets/burger.js
@@ -1,4 +1,5 @@
 $(function () {
+    //on add a burger, we are giving that value of not devoured for it to display in not devoured section 
   $(".create-form").on("submit", function (event) {
     //    need to prevent default in form
     event.preventDefault();
@@ -6,6 +7,7 @@ $(function () {
       burger_name: $("#newBurger").val().trim(),
       devoured: 0,
     };
+    // by using post method, can create a new burger
     $.ajax("api/burgers", {
         type:"POST",
         data: createdBurger
@@ -14,5 +16,23 @@ $(function () {
         location.reload()
     })
 
+  });
+//Once user clicks "DEVOUR IT", the burger will be changed to devoured true
+  $("#devourButton").on("click", function(event) {
+    //need to prevent default in form
+    event.preventDefault();
+
+    var id = $(this).data("id");
+    var devouredState = {
+      devoured: 1
+    };
+// by using put method, burger is updated 
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: devouredState
+    }).then(function() {
+      console.log("Burger devoured");
+      location.reload();
+    });
   });
 });
