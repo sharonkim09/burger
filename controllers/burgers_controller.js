3a2d384e98cc7e9b976c1b92391a573681682664
Sharon Kim <sharonkim1709@gmail.com>
2020-07-14 02:04:59 -0400

    created controller for router.get to display all burgers

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var burger = require("../models/burger");
+
+var router = express.Router();
+
+router.get("/", function(req, res) {
+  burger.selectAll(function(data) {
+    var hdbrsObj = {
+      burgers: data
+    };
+    console.log(hdbrsObj);
+    res.render("index", hdbrsObj);
+  });
+});
+  module.exports = router;
