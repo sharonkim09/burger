23500e413b782a833619ff33386d2e173af69dcc
Sharon Kim <sharonkim1709@gmail.com>
2020-07-14 08:40:44 -0400

    added update sql statement

--- config/orm.js
+++ config/orm.js
@@ -69,6 +69,24 @@ var orm = {
           cb(result);
         });
       },
+        // An example of objColVals would be {name: panther, sleepy: true}
+      updateOne: function(table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table;
+    
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+    
+        console.log(queryString);
+        connection.query(queryString, function(err, result) {
+          if (err) {
+            throw err;
+          }
+    
+          cb(result);
+        });
+      },
 
 
 }
