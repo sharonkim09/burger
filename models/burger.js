77e5eb1245b7405eef7404f901dbc570f53561bc
Sharon Kim <sharonkim1709@gmail.com>
2020-07-14 08:49:12 -0400

    added update router orm func in model

--- models/burger.js
+++ models/burger.js
@@ -12,6 +12,11 @@ var burger = {
           cb(res);
         });
       },
+      updateOne: function(objColVals, condition, cb) {
+        orm.updateOne("burgers", objColVals, condition, function(res) {
+          cb(res);
+        });
+      },
 };
 
 module.exports = burger;
